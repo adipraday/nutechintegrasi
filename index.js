f7ea4de3ae8eac7dfc26157a8bc016fdd0703229
adipraday <adipradayozan@gmail.com>
2025-02-07 12:42:51 +0700

    commit5, revisi-2 main index.js - perubahan port

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import router from "./routes/index.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env;
+const PORT = process.env.PORT || 5000;
 
 const connectDB = async () => {
   try {
